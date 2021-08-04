815ad7459c5bb5d04472a5795a88a0b1f16dc975
huarxia <liubiao0810@live.cn>
2021-08-04 13:15:03 +0800

    feat: 🌹 初始化左侧菜单

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,35 @@
 <template>
-    <el-radio-group v-model="isCollapse" style="margin-bottom: 20px;">
-        <el-radio-button :label="false">展开</el-radio-button>
-        <el-radio-button :label="true">收起</el-radio-button>
-    </el-radio-group>
-    <el-menu default-active="1-4-1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" :collapse="isCollapse">
-        <el-submenu index="1">
-            <template #title>
-                <i class="el-icon-location"></i>
-                <span>导航一</span>
-            </template>
-            <el-menu-item-group>
-                <template #title>分组一</template>
-                <el-menu-item index="1-1">选项1</el-menu-item>
-                <el-menu-item index="1-2">选项2</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="分组2">
-                <el-menu-item index="1-3">选项3</el-menu-item>
-            </el-menu-item-group>
-            <el-submenu index="1-4">
-                <template #title>选项4</template>
-                <el-menu-item index="1-4-1">选项1</el-menu-item>
-            </el-submenu>
-        </el-submenu>
-        <el-menu-item index="2">
-            <i class="el-icon-menu"></i>
-            <template #title>导航二</template>
-        </el-menu-item>
-        <el-menu-item index="3" disabled>
-            <i class="el-icon-document"></i>
-            <template #title>导航三</template>
-        </el-menu-item>
-        <el-menu-item index="4">
-            <i class="el-icon-setting"></i>
-            <template #title>导航四</template>
-        </el-menu-item>
-    </el-menu>
+<el-row :gutter="10" class="container">
+    <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="3">
+        <LeftMenu />
+    </el-col>
+</el-row>
 </template>
 <script>
+import LeftMenu from '@component/left-menu';
 export default {
     name: 'App',
-    components: {}
+    components: {
+        LeftMenu
+    }
 };
 </script>
 
-<style>
+<style lang="scss">
+body {
+    height: 100vh;
+    margin: 0;
+    overflow-x: hidden;
+}
 #app {
-    margin-top: 60px;
+    height: 100%;
     font-family: Avenir, Helvetica, Arial, sans-serif;
     text-align: center;
     color: #2c3e50;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+    .container {
+        height: 100%;
+    }
 }
 </style>
---
+++ src/components/left-menu/index.vue
@@ -0,0 +1,7 @@
+<template src="./index.tpl.html"></template>
+<script>
+export default {
+
+};
+</script>
+<style src="./index.scss" lang="scss" scoped></style>
